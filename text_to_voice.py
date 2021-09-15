8f01c7f26225d4e8d1fb59badb93140b528aa156
Pratyush <pratyushjha06@gmail.com>
2021-09-15 20:09:20 +0530

    Initial commit

--- text_to_voice.py
+++ text_to_voice.py
@@ -1,9 +1,8 @@
-# This has been tryed by PJ
 import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
-# print(voices[1].id) :for girl. voice 
+# print(voices[1].id) :for girl voice 
 engine.setProperty('rate', 130)
 engine.say("Hello World")
 engine.runAndWait()
